f26be8ef4a822bec3d1adbb710c9c1acb732e31a
DataOmbudsman <data.ombudsman@tutanota.com>
2022-12-06 19:50:49 +0100

    Add day06 solutions
    Signed-off-by: DataOmbudsman <data.ombudsman@tutanota.com>

---
+++ day-06/part1.py
@@ -0,0 +1,25 @@
+def _are_different_chars(chars):
+    for i, char1 in enumerate(chars):
+        for j, char2 in enumerate(chars):
+            if i != j and char1 == char2:
+                return False
+    return True
+
+
+def find_marker(line, marker_length):
+    i = 0
+    while not _are_different_chars(line[i: i+marker_length]):
+        i += 1
+    return i + marker_length
+
+
+def main():
+    with open('input') as lines:
+        line = lines.readline()
+
+    marker = find_marker(line, 4)
+    print(marker)
+
+
+if __name__ == "__main__":
+    main()
